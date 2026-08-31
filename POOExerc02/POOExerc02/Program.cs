bf69ccbfccab8d2eb7aa55700efc41701d20b2c1
Guimascena <gmascena.odm@gmail.com>
2026-08-31 20:27:22 -0300

    Add files via upload

---
+++ POOExerc02/POOExerc02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado1 = new Quadrado();
+
+            quadrado1.setAresta(entrarInt("Digite o valor da  aresta do quadrado: "));
+            quadrado1.calcular();
+
+            Console.WriteLine("A área do quadrado com aresta {0} é {1}",
+                quadrado1.getAresta(),
+                quadrado1.getArea());
+        }
+
+        static int entrarInt(string mensagem)
+        {
+            Console.WriteLine(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ POOExerc02/POOExerc02/Quadrado.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc02
+{
+    internal class Quadrado
+    {
+        // Atributos
+        private int aresta;
+        private int area;
+
+
+        // Métodos Construtores
+        public Quadrado()
+        {
+            this.aresta = 0;
+            this.area = 0;
+        }
+
+        public Quadrado(int aresta)
+        {
+            this.aresta= aresta;
+            this.area = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setAresta(int aresta)
+        {
+            this.aresta = aresta;
+        }
+
+        public int getAresta()
+        {
+            return this.aresta;
+        }
+
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        // Método funcional
+        public void calcular()
+        {
+            this.area = this.aresta * this.aresta;
+        }
+
+    }
+}
