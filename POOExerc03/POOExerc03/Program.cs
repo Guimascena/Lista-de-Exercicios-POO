0c58defc44afc50aeb205dccc9e415078f5cc752
Guimascena <gmascena.odm@gmail.com>
2026-08-31 20:57:35 -0300

    Add files via upload

---
+++ POOExerc03/POOExerc03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadrado1;
+            quadrado1 = new Quadrado();
+            quadrado1.setDiagonal(entrarDouble("Digite o valor da diagonal do quadrado: "));
+
+            quadrado1.calcular();
+
+            Console.WriteLine("A área do quadrado com diagonal {0} é {1}",
+                quadrado1.getDiagonal(),
+                quadrado1.getArea());
+        }
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+
+    }
+}
