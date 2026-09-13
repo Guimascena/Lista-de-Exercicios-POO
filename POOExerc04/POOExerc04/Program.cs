8268e4c5365c24d94d87c92b9f0fa77fec289c76
Guimascena <gmascena.odm@gmail.com>
2026-09-13 14:36:50 -0300

    Add files via upload

---
+++ POOExerc04/POOExerc04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1;
+            triangulo1 = new Triangulo();
+
+            triangulo1.setBase(entrarInt("Digite a base do triângulo: "));
+            triangulo1.setAltura(entrarInt("Digite a altura do triângulo: "));
+
+            triangulo1.calcular();
+
+            Console.WriteLine("A área do triângulo com base {0} e altura {1} é {2}",
+                triangulo1.getBase(),
+                triangulo1.getAltura(),
+                triangulo1.getArea());
+
+        }
+
+        static int entrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ POOExerc04/POOExerc04/Triangulo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc04
+{
+    internal class Triangulo
+    {
+        // Atributos
+        private int base_;
+        private int altura;
+        private double area;
+
+        // Métodos Construtores
+        public Triangulo()
+        {
+            this.base_ = 0;
+            this.altura = 0;
+            this.area = 0;
+        }
+
+        public Triangulo(int base_, int altura)
+        {
+            this.base_ = base_;
+            this.altura = altura;
+            this.area = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setBase(int base_)
+        {
+            this.base_ = base_;
+        }
+
+        public void setAltura(int altura)
+        {
+            this.altura = altura;
+        }
+
+        public int getBase()
+        {
+            return this.base_;
+        }
+
+        public int getAltura()
+        {
+            return this.altura;
+        }
+
+        public double getArea()
+        {
+            return this.area;
+        }
+
+        // Método funcional
+        public void calcular()
+        {
+            this.area = (this.base_ * this.altura) / 2.0;
+        }
+    }
+}
