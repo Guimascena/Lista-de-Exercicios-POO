1efe0a9cb5b2e406a6e0473bdc25f9fc2f456194
Guimascena <gmascena.odm@gmail.com>
2026-09-13 15:00:25 -0300

    Add files via upload

---
+++ POOExerc05/POOExerc05/MilhaMaritima.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc05
+{
+    internal class MilhaMaritima
+    {
+        // Atributos
+        private double milhas;
+        private double quilometros;
+
+        // Métodos construtores
+        public MilhaMaritima()
+        {
+            this.milhas = 0;
+            this.quilometros = 0;
+        }
+
+        public MilhaMaritima(double milhas)
+        {
+            this.milhas = milhas;
+            this.quilometros = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setMilhas(double milhas)
+        {
+            this.milhas = milhas;
+        }
+
+        public double getMilhas()
+        {
+            return this.milhas;
+        }
+
+        public double getQuilometros()
+        {
+            return this.quilometros;
+        }
+
+        // Método funcional
+        public void converter()
+        {
+            this.quilometros = (this.milhas * 1852) / 1000;
+        }
+    }
+}
---
+++ POOExerc05/POOExerc05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MilhaMaritima milha1;
+            milha1 = new MilhaMaritima();
+
+            milha1.setMilhas(entrarDouble("Digite o valor em milhas marítimas: "));
+
+            milha1.converter();
+
+            Console.WriteLine("{0} milhas marítimas equivalem a {1} quilômetros",
+                milha1.getMilhas(),
+                milha1.getQuilometros());
+        }
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
