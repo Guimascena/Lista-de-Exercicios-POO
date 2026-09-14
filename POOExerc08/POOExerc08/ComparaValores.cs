07ea6e0cc7dfd29b16f3f9b62ddffac94ea89426
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:13:08 -0300

    Add files via upload

---
+++ POOExerc08/POOExerc08/ComparaValores.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc08
+{
+    internal class ComparaValores
+    {
+        // Atributos
+        private int valor1;
+        private int valor2;
+        private int maior;
+        private bool saoIguais;
+
+        // Métodos construtores
+        public ComparaValores()
+        {
+            this.valor1 = 0;
+            this.valor2 = 0;
+            this.maior = 0;
+            this.saoIguais = false;
+        }
+
+        public ComparaValores(int valor1, int valor2)
+        {
+            this.valor1 = valor1;
+            this.valor2 = valor2;
+            this.maior = 0;
+            this.saoIguais = false;
+        }
+
+        // Métodos de interface (set/get)
+        public void setValor1(int valor1)
+        {
+            this.valor1 = valor1;
+        }
+
+        public void setValor2(int valor2)
+        {
+            this.valor2 = valor2;
+        }
+
+        public int getValor1()
+        {
+            return this.valor1;
+        }
+
+        public int getValor2()
+        {
+            return this.valor2;
+        }
+
+        public int getMaior()
+        {
+            return this.maior;
+        }
+
+        public bool getSaoIguais()
+        {
+            return this.saoIguais;
+        }
+
+        // Método funcional
+        public void comparar()
+        {
+            if (this.valor1 == this.valor2)
+            {
+                this.saoIguais = true;
+            }
+            else
+            {
+                this.saoIguais = false;
+
+                if (this.valor1 > this.valor2)
+                {
+                    this.maior = this.valor1;
+                }
+                else
+                {
+                    this.maior = this.valor2;
+                }
+            }
+        }
+    }
+}
---
+++ POOExerc08/POOExerc08/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComparaValores comparacao1;
+            comparacao1 = new ComparaValores();
+
+            comparacao1.setValor1(entrarInt("Digite o 1º valor: "));
+            comparacao1.setValor2(entrarInt("Digite o 2º valor: "));
+
+            comparacao1.comparar();
+
+            if (comparacao1.getSaoIguais())
+            {
+                Console.WriteLine("Os números são idênticos ({0} e {1})",
+                    comparacao1.getValor1(),
+                    comparacao1.getValor2());
+            }
+            else
+            {
+                Console.WriteLine("Entre {0} e {1}, o maior valor é {2}",
+                    comparacao1.getValor1(),
+                    comparacao1.getValor2(),
+                    comparacao1.getMaior());
+            }
+        }
+
+        static int entrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
