3db50c46d2a479dffe972ad1f3892631b3c55b2a
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:14:29 -0300

    Add files via upload

---
+++ POOExerc10/POOExerc10/IndiceMassaCorporal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc10
+{
+    internal class IndiceMassaCorporal
+    {
+        // Atributos
+        private double peso;
+        private double altura;
+        private double indice;
+
+        // Métodos construtores
+        public IndiceMassaCorporal()
+        {
+            this.peso = 0;
+            this.altura = 0;
+            this.indice = 0;
+        }
+
+        public IndiceMassaCorporal(double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+            this.indice = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setPeso(double peso)
+        {
+            this.peso = peso;
+        }
+
+        public void setAltura(double altura)
+        {
+            this.altura = altura;
+        }
+
+        public double getPeso()
+        {
+            return this.peso;
+        }
+
+        public double getAltura()
+        {
+            return this.altura;
+        }
+
+        public double getIndice()
+        {
+            return this.indice;
+        }
+
+        // Método funcional
+        public void calcular()
+        {
+            this.indice = this.peso / (this.altura * this.altura);
+        }
+    }
+}
---
+++ POOExerc10/POOExerc10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IndiceMassaCorporal pessoa1;
+            pessoa1 = new IndiceMassaCorporal();
+
+            pessoa1.setPeso(entrarDouble("Digite o peso (em kg): "));
+            pessoa1.setAltura(entrarDouble("Digite a altura (em metros): "));
+
+            pessoa1.calcular();
+
+            Console.WriteLine("O índice (R) calculado foi {0}", pessoa1.getIndice());
+
+            if (pessoa1.getIndice() < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            }
+            else if (pessoa1.getIndice() < 25)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else
+            {
+                Console.WriteLine("Acima do peso");
+            }
+        }
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
