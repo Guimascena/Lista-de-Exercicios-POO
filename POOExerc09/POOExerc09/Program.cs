2f54404df4e904401612ecfef802c9afd8a6fef2
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:13:57 -0300

    Add files via upload

---
+++ POOExerc09/POOExerc09/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo1;
+            retangulo1 = new Retangulo();
+
+            retangulo1.setBase(entrarInt("Digite a base do retângulo: "));
+            retangulo1.setAltura(entrarInt("Digite a altura do retângulo: "));
+
+            retangulo1.calcular();
+
+            Console.WriteLine("A área do retângulo com base {0} e altura {1} é {2}",
+                retangulo1.getBase(),
+                retangulo1.getAltura(),
+                retangulo1.getArea());
+
+            if (retangulo1.getArea() > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+
+        static int entrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ POOExerc09/POOExerc09/Retangulo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc09
+{
+    internal class Retangulo
+    {
+        // Atributos
+        private int base_;
+        private int altura;
+        private int area;
+
+        // Métodos construtores
+        public Retangulo()
+        {
+            this.base_ = 0;
+            this.altura = 0;
+            this.area = 0;
+        }
+
+        public Retangulo(int base_, int altura)
+        {
+            this.base_ = base_;
+            this.altura = altura;
+            this.area = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setBase(int base_)
+        {
+            this.base_ = base_;
+        }
+
+        public void setAltura(int altura)
+        {
+            this.altura = altura;
+        }
+
+        public int getBase()
+        {
+            return this.base_;
+        }
+
+        public int getAltura()
+        {
+            return this.altura;
+        }
+
+        public int getArea()
+        {
+            return this.area;
+        }
+
+        // Método funcional
+        public void calcular()
+        {
+            this.area = this.base_ * this.altura;
+        }
+    }
+}
