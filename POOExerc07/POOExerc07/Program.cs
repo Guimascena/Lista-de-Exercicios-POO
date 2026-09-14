f5c476036baa60522c4b395c6915279c8f0653bc
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:12:38 -0300

    Add files via upload

---
+++ POOExerc07/POOExerc07/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComparaValores comparacao1;
+            comparacao1 = new ComparaValores();
+
+            comparacao1.setValor1(entrarInt("Digite o 1º valor: "));
+            comparacao1.setValor2(entrarInt("Digite o 2º valor: "));
+
+            comparacao1.comparar();
+
+            Console.WriteLine("Entre {0} e {1}, o maior valor é {2}",
+                comparacao1.getValor1(),
+                comparacao1.getValor2(),
+                comparacao1.getMaior());
+        }
+
+        static int entrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
