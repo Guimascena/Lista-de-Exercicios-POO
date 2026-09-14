bbb0f963a1a195423f68ea2435030c4ea9d64d2d
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:15:01 -0300

    Add files via upload

---
+++ POOExerc11/POOExerc11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo1;
+            triangulo1 = new Triangulo();
+
+            triangulo1.setLado1(entrarDouble("Digite o 1º lado: "));
+            triangulo1.setLado2(entrarDouble("Digite o 2º lado: "));
+            triangulo1.setLado3(entrarDouble("Digite o 3º lado: "));
+
+            triangulo1.verificar();
+
+            if (triangulo1.getFormaTriangulo())
+            {
+                Console.WriteLine("Os valores formam um triângulo {0}", triangulo1.getClassificacao());
+            }
+            else
+            {
+                Console.WriteLine("Os valores não formam um triângulo");
+            }
+        }
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
