9a51c11b467cc4cfb355dc6c158d106ad1631951
Guimascena <gmascena.odm@gmail.com>
2026-09-13 23:12:02 -0300

    Add files via upload

---
+++ POOExerc06/POOExerc06/ConversaoDolar.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc06
+{
+    internal class ConversaoDolar
+    {
+        // Atributos
+        private double cotacao;
+        private double quantidadeDolares;
+        private double valorReais;
+
+        // Métodos construtores
+        public ConversaoDolar()
+        {
+            this.cotacao = 0;
+            this.quantidadeDolares = 0;
+            this.valorReais = 0;
+        }
+
+        public ConversaoDolar(double cotacao, double quantidadeDolares)
+        {
+            this.cotacao = cotacao;
+            this.quantidadeDolares = quantidadeDolares;
+            this.valorReais = 0;
+        }
+
+        // Métodos de interface (set/get)
+        public void setCotacao(double cotacao)
+        {
+            this.cotacao = cotacao;
+        }
+
+        public void setQuantidadeDolares(double quantidadeDolares)
+        {
+            this.quantidadeDolares = quantidadeDolares;
+        }
+
+        public double getCotacao()
+        {
+            return this.cotacao;
+        }
+
+        public double getQuantidadeDolares()
+        {
+            return this.quantidadeDolares;
+        }
+
+        public double getValorReais()
+        {
+            return this.valorReais;
+        }
+
+        // Método funcional
+        public void converter()
+        {
+            this.valorReais = this.quantidadeDolares * this.cotacao;
+        }
+    }
+}
---
+++ POOExerc06/POOExerc06/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOExerc06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversaoDolar conversao1;
+            conversao1 = new ConversaoDolar();
+
+            conversao1.setCotacao(entrarDouble("Digite a cotação do dólar: "));
+            conversao1.setQuantidadeDolares(entrarDouble("Digite a quantidade de dólares: "));
+
+            conversao1.converter();
+
+            Console.WriteLine("{0} dólares, com cotação de R$ {1}, equivalem a R$ {2}",
+                conversao1.getQuantidadeDolares(),
+                conversao1.getCotacao(),
+                conversao1.getValorReais());
+        }
+
+        static double entrarDouble(string mensagem)
+        {
+            Console.Write(mensagem);
+            return double.Parse(Console.ReadLine());
+        }
+    }
+}
